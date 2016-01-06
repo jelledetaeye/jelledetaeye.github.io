1116d9c60ae4aecb4e5b2fcee5239c7a10feb938
Jelle Decock <jelle.decock@liferay.com>
2016-01-06 22:10:28 +0000

    update media queries

--- js/script.js
+++ js/script.js
@@ -1,22 +1,27 @@
 var app = {
 	loadContent: function() {
-		$("#headerSection").load("includes/header.html");				
-		$("#footerSection").load("includes/footer.html");
+		$("#headerSection").load("includes/header.html", function() {
+      //Set Listener
+      $("#navbar a").on('click', function() {
+  			$("#navbar").find('li').removeClass('active');
+  			$(this).parent().addClass('active');
+  		});
+    });
+		$("#footerSection").load("includes/footer.html", function() {
+      //Set Listener
+      $("footer button").on('click', function() {
+        $(window).scrollTop(0);
+      });
+    });
 		$("#aboutSection").load("includes/about.html");
 		$("#blogSection").load("includes/publications.html");
 		$("#projectsSection").load("includes/projects.html");
 		$("#socialSection").load("includes/social.html");
-		$("#raceResultsSection").load("includes/race-results.html");
-	},
-
-	setListeners: function() {
-		$("#navbar a").on('click', function() {
-			$("#navbar").find('li').removeClass('active');
-			$(this).parent().addClass('active');
-		});
-
-		$("footer button").on('click', function() {
-			$(window).scrollTop(0);
-		});
+		$("#raceResultsSection").load("includes/race-results.html", function (){
+      //Set Listener
+      $("#raceResultsBtn").on('click', function() {
+        $(".raceResultsContent").toggle();
+      });
+    });
 	}
 };
